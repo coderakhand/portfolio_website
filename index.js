b25ae7fb98d0e2d3365f9a2cabcc10e1d6d244a6
coderakhand <akhandpratap0027+github@gmail.com>
2023-09-30 13:33:21 +0530

    added js file and modified css

---
+++ index.js
@@ -0,0 +1,9 @@
+function removeflip() {
+  document.getElementById("instagram").classList.remove("fa-flip");
+}
+function removebounce_facebook() {
+  document.getElementById("facebook").classList.remove("fa-bounce");
+}
+function removebounce_linkedin() {
+  document.getElementById("linkedin").classList.remove("fa-bounce");
+}
