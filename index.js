4dfefeb6268cbeb4f87bdfac88d5c231fdd6fc18
coderakhand <akhandpratap0027+github@gmail.com>
2023-10-02 13:42:58 +0530

    removed 2 functions

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 function removeflip() {
   document.getElementById("instagram").classList.remove("fa-flip");
 }
-function removebounce_facebook() {
-  document.getElementById("facebook").classList.remove("fa-bounce");
-}
-function removebounce_linkedin() {
-  document.getElementById("linkedin").classList.remove("fa-bounce");
-}
