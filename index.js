0039e20d086792b9a7cb152a433fe7491c23d4cb
coderakhand <akhandpratap0027+github@gmail.com>
2023-10-17 08:50:08 +0530

    added new features

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function removeflip() {
   document.getElementById("instagram").classList.remove("fa-flip");
 }
+function avtaar2real(){
+document.getElementsByClassName("my-image")[0].src="akhand-profile.png";
+document.getElementById("akhand-image").classList.remove("avtaar-image");
+document.getElementById("akhand-image").classList.add("real-image");
+}
+function real2avtaar(){
+  document.getElementsByClassName("my-image")[0].src="akhand.jpg";
+  document.getElementById("akhand-image").classList.remove("real-image");
+  document.getElementById("akhand-image").classList.add("avtaar-image"); 
+}
